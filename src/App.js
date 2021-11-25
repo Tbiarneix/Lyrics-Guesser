2b737597d8aeb86ce9aac771f214a60dd9967308
Cecile <cecile.lehoux@laposte.net>
2021-11-25 16:50:28 +0100

    creation css et recuperation code de la home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import TestVocal from './components/TestVocal';
+import Home from './components/page/Home';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Home />
      <TestVocal />
       </header>
     </div>
--- src/components/page/Home.jsx
+++ src/components/page/Home.jsx
@@ -2,7 +2,20 @@ import '../style/Home.css';
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <div>Home
+        <h1>Règles du jeu</h1>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+            when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+            It has survived not only five centuries, but also the leap into electronic typesetting, 
+            remaining essentially unchanged. It was popularised in the 1960s with the release of 
+            Letraset sheets containing Lorem Ipsum passages, and more recently </p>
+
+            <button type="button">Jouer</button>
+        <div>
+            <h2>Envoyer l'application à un ami</h2>
+        </div>
+        </div>
     );
 }
 
