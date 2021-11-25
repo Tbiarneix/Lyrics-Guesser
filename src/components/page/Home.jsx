b281edc9b27b929bc5c5de1c20811d20b86fe3c6
MAMA Anas <anas.mama@hotmail.com>
2021-11-25 16:12:39 +0100

    ajustement règle

--- src/components/page/Home.jsx
+++ src/components/page/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
     return (
         <div>
             <h1>Règle du jeu</h1>
-            <p>Les paroles de 10 musiques vont être lues. Vous avez 30 secondes pour trouver le nom et l'artiste de chaque musique. Prêt à jouer ?</p>
+            <p>Les paroles de 10 musiques vont être lues à haute voix. Vous avez 30 secondes pour trouver le nom et l'artiste de chaque musique. Prêt à jouer ?</p>
             {/* bouton = link to 'Game.jsx' */}
             <button type="button">Jouer</button>
         </div>
