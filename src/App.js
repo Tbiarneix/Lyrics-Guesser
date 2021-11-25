88a94578bde0b3d31da8e9995b6b56940ac3ef1a
MAMA Anas <anas.mama@hotmail.com>
2021-11-25 11:48:42 +0100

    structure de base

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Home from './components/page/Home';
+import Game from './components/page/Game';
+import Result from './components/page/Result';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <Header/>
+        <Home/>
+        <Footer/>
+        <Game/>
+        <Result/>
       </header>
     </div>
   );
