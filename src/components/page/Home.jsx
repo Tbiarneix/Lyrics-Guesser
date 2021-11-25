dcaa1a3bbcf4affb119845ecd816a2d71c3e6dca
MAMA Anas <anas.mama@hotmail.com>
2021-11-25 15:55:13 +0100

    ajout éléments de base home et result

--- src/components/page/Home.jsx
+++ src/components/page/Home.jsx
@@ -2,7 +2,12 @@ import '../style/Home.css';
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <div>
+            <h1>Règle du jeu</h1>
+            <p>Les paroles de 10 musiques vont être lues. Vous avez 30 secondes pour trouver le nom et l'artiste de chaque musique. Prêt à jouer ?</p>
+            {/* bouton = link to 'Game.jsx' */}
+            <button type="button">Jouer</button>
+        </div>
     );
 }
 
--- src/components/page/Result.jsx
+++ src/components/page/Result.jsx
@@ -1,8 +1,29 @@
+// import { useState } from 'react';
 import '../style/Result.css';
 
-const Result = () => {
+const Result = ({ score }) => {
+    // const [congratulation, setCongratulation] = useState("");
+    let congratulation = "";
+
+    if (score == 10) {
+        congratulation = "Parfait ! Tu es beaucoup trop chaud !"   
+    } else if (score > 6) {
+        congratulation = "Tu es balèze de ouf ! Bravo !"     
+    } else if (score > 4) {
+        congratulation = "Plutôt pas mal ! Bien joué !"    
+    } else if (score > 2) {
+        congratulation = "C'est pas terrible... Tu feras mieux la prochaine fois !"  
+    } else {
+        congratulation = "Nul ! Nul ! Nul ! Tu es obligé de rejouer pour laver cet échec !"     
+    };
+
     return (
-        <div>Result</div>
+        <div>
+            <h1>Vous avez répondu correctement à {score}/10 questions !</h1>
+            <h2>{congratulation}</h2>
+            {/* bouton = link to 'Game.jsx' */}
+            <button type="button">Rejouer</button>
+        </div>
     );
 }
 
