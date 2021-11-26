69718b2369399a6213ec4f7fb7c2a073bd0d33a8
Antoniatch <antoniatch@zohomail.eu>
2021-11-26 11:05:27 +0100

    tracks component

---
+++ src/components/data/Tracks.js
@@ -0,0 +1,22 @@
+const Tracks = [
+    {
+        singer: 'Beyoncé',
+        track: 'Irreplaceable',
+        lyric: "To the left, to the left, everything you own in the bow to the left, in the closet, that's my stuff, yes, if I bought it, please don't touch, and keep talking that mess that's fine, But could you walk and talk at the same time? And, it's my name that's on that Jag, So remove your bags, let me call you a cab, Standing in the front you're telling me how I'm such a fool, talking 'bout how I'll never find a man like you, you got me twisted"
+    },    {
+        singer: 'Ed Sheeran',
+        track: 'Shape of you',
+        lyric: "The club isn't the best place to find a lover, So the bar is where I go (mmmm), Me and my friends at the table doing shots, Drinking fast and then we talk slow (mmmm), And you come over and start up a conversation with just me, And trust me I'll give it a chance now (mmmm)Take my hand, stop, put Van The Man on the jukebox, And then we start to dance, And now I'm singing like, Girl, you know I want your love, Your love was handmade for somebody like me, Come on now, follow my lead"
+    },    {
+        singer: 'Red Hot Chili Peppers',
+        track: "Can't stop",
+        lyric: "Can't stop, addicted to the shindig, Chop Top, he says I'm gonna win big, Choose not a life of imitation, Distant cousin to the reservation, Defunct, the pistol that you pay for, This punk, the feelin' that you stay for, In time I want to be your best friend, East side love is living on the West End, Knocked out, but, boy, you better come to (Oh, oh-oh), Don't die, you know, the truth as some do (Oh-oh), Go write your message on the pavement (Oh-oh), Burn so bright, I wonder what the wave meant"
+    },
+    {
+        singer: "Daft Punk",
+        track: "Harder, better, faster, stronger",
+        lyric: "Work it, make it, do it, makes us, Harder, better, faster, stronger, More than, hour, hour, never, Ever, after, work is over, Work it, make it, do it, makes us, Harder, better, faster, stronger, Work it harder, make it better, Do it faster, makes us stronger, More than ever, hour after, Hour, work is never over, Work it harder, make it better, Do it faster, makes us stronger, More than ever, hour after, Hour, work is never over, Work it harder, make it better, Do it faster, makes us stronger, More than ever, hour after, Hour, work is never over"
+    }
+]
+
+export default Tracks;
