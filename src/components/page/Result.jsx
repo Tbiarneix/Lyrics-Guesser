98970ba7a2c9897dcd4c9c52de965a7fa28a8957
Tbiarneix <thomas.biarneix@gmail.com>
2021-11-27 12:03:10 +0100

    finalisation de la page results

--- src/components/page/Result.jsx
+++ src/components/page/Result.jsx
@@ -7,17 +7,23 @@ import '../style/Home.css';
 const Result = ({ totalScore }) => {
     // const [congratulation, setCongratulation] = useState("");
     let congratulation = "";
+    let scoreImage = "";
 
     if (totalScore === 100) {
-        congratulation = "Parfait ! Tu es beaucoup trop chaud !"   
+        congratulation = "Parfait ! Tu es beaucoup trop chaud !"
+        scoreImage = "/assets/100plus.gif"
     } else if (totalScore > 59) {
-        congratulation = "Tu es balèze de ouf ! Bravo !"     
+        congratulation = "Tu es balèze de ouf ! Bravo !"
+        scoreImage = "/assets/60plus.gif"  
     } else if (totalScore > 39) {
-        congratulation = "Plutôt pas mal ! Bien joué !"    
+        congratulation = "Plutôt pas mal ! Bien joué !"
+        scoreImage = "/assets/40plus.gif"    
     } else if (totalScore > 19) {
-        congratulation = "C'est pas terrible... Tu feras mieux la prochaine fois !"  
+        congratulation = "C'est pas terrible... Tu feras mieux la prochaine fois !"
+        scoreImage = "/assets/20plus.gif" 
     } else {
-        congratulation = "Nul ! Nul ! Nul ! Tu es obligé de rejouer pour laver cet échec !"     
+        congratulation = "Nul ! Nul ! Nul ! Tu es obligé de rejouer pour laver cet échec !" 
+        scoreImage = "/assets/0plus.gif"    
     };
 
     return (
@@ -26,6 +32,7 @@ const Result = ({ totalScore }) => {
             <div className="container">
             <h1>Votre score est de {totalScore} !</h1>
             <h2>{congratulation}</h2>
+            <div><img src={scoreImage} alt='scoreImage' /></div>
             <NavLink exact to='/game'><button className="play" type="button" >Rejouer</button></NavLink>
             </div>
         </div>
